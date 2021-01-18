afac6442b7583b0e5b5409206e2232d7762c9e03
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-18 12:25:41 -0600

    added new route from home page to room page

--- room_controller/frontend/src/components/HomePage.js
+++ room_controller/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 export default class HomePage extends Component {
@@ -14,8 +15,9 @@ export default class HomePage extends Component {
             <Router>
                 <Switch>
                     <Route exact path='/'><p>This is the Home Page.</p></Route>
-                    <Route path='/join' component={ RoomJoinPage }/>
-                    <Route path='/create' component={ CreateRoomPage }/>
+                    <Route path='/join' component={RoomJoinPage}/>
+                    <Route path='/create' component={CreateRoomPage}/>
+                    <Route path="/room/:roomCode" component={Room}></Route>
                 </Switch>
             
             </Router>
