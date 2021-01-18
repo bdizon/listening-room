e0f53d750827db6b87817de8d6e7630064ecea37
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-18 12:24:25 -0600

    new url path to room page

--- room_controller/frontend/urls.py
+++ room_controller/frontend/urls.py
@@ -5,5 +5,6 @@ from .views import index
 urlpatterns = [
     path('', index),  # render index template whenever there is a blank path, "homepage"
     path('join', index),
-    path('create', index)
+    path('create', index),
+    path('room/<str:roomCode>', index)  # dynamic url, accept any string
 ]
