9c974074a43d17bb75be5b16ae1a66818ba4929d
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-18 16:07:20 -0600

    added url path for api/join-room view

--- room_controller/api/urls.py
+++ room_controller/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 urlpatterns = [
     path('room', RoomView.as_view()),   # take class and give view, shows the rooms i the DB
     # path('', main) # cant have functions and classes being views
     path('create-room', CreateRoomView.as_view()),  # shows the create-room
-    path('get-room', GetRoom.as_view()) # shows the info of the room 
+    path('get-room', GetRoom.as_view()), # shows the info of the room 
+    path('join-room', JoinRoom.as_view())   # join room view
     
 ]
