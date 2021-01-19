5c478e823de4f4b41a05c444b82aac8a27333418
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-19 13:23:49 -0600

    set up url path to user-in-room

--- room_controller/api/urls.py
+++ room_controller/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
 urlpatterns = [
     path('room', RoomView.as_view()),   # take class and give view, shows the rooms i the DB
     # path('', main) # cant have functions and classes being views
     path('create-room', CreateRoomView.as_view()),  # shows the create-room
     path('get-room', GetRoom.as_view()), # shows the info of the room 
-    path('join-room', JoinRoom.as_view())   # join room view
+    path('join-room', JoinRoom.as_view()),   # join room view
+    path('user-in-room', UserInRoom.as_view()), # shows if user is currently in a room
     
 ]
