0939ccd79f765190a34d85733a246d3967ef925c
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-19 13:50:04 -0600

    fixed misspelling of class Room component

--- room_controller/frontend/src/components/Room.js
+++ room_controller/frontend/src/components/Room.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Roon extends Component {
+export default class Room extends Component {
     constructor(props) {
         super(props);
         this.state = {
