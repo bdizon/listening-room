ee1e8cd748f6bc3a0e5658393a30a1c822512c07
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-19 17:18:34 -0600

    added "leave-room" path

--- room_controller/api/urls.py
+++ room_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 urlpatterns = [
     path('room', RoomView.as_view()),   # take class and give view, shows the rooms i the DB
     # path('', main) # cant have functions and classes being views
@@ -7,5 +7,5 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()), # shows the info of the room 
     path('join-room', JoinRoom.as_view()),   # join room view
     path('user-in-room', UserInRoom.as_view()), # shows if user is currently in a room
-    
+    path('leave-room', LeaveRoom.as_view()),    # shows user left room
 ]
