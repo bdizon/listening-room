1211970c3ea70c18aeb3501bf3e2a68bc42141df
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-20 21:57:58 -0600

    added update-room path

--- room_controller/api/urls.py
+++ room_controller/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
+
 urlpatterns = [
     path('room', RoomView.as_view()),   # take class and give view, shows the rooms i the DB
     # path('', main) # cant have functions and classes being views
@@ -8,4 +9,5 @@ urlpatterns = [
     path('join-room', JoinRoom.as_view()),   # join room view
     path('user-in-room', UserInRoom.as_view()), # shows if user is currently in a room
     path('leave-room', LeaveRoom.as_view()),    # shows user left room
+    path('update-room', UpdateRoom.as_view()),  # update the room setting
 ]
