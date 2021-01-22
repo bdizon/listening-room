2d1d45b1ed8c47d64ad3f43621c9369d50616e17
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-22 17:06:27 -0600

    added app_name to distinguish the frontend app
    redirect to homepage of 'frontend' after authentication

--- room_controller/frontend/urls.py
+++ room_controller/frontend/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import index
 
+app_name = 'frontend' #django needs to know url.py file belongs to frontend app
 
 urlpatterns = [
-    path('', index),  # render index template whenever there is a blank path, "homepage"
+    path('', index, name=''),  # render index template whenever there is a blank path, "homepage"
     path('join', index),
     path('create', index),
     path('room/<str:roomCode>', index)  # dynamic url, accept any string
