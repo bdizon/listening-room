eac920aabe921c7a35522be0a4eabb0760f30963
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-22 19:46:26 -0600

    fixed bug: missing indent in renderSettings
    caused the number of votes not to reflect the correct amount

--- room_controller/frontend/src/components/Room.js
+++ room_controller/frontend/src/components/Room.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
-import { Grid, Button, Typography } from "@material-ui/core";
-import { Link }  from "react-router-dom";
+import React, { Component } from "react";
+import {
+  Grid,
+  Button,
+  Typography,
+  responsiveFontSizes,
+} from "@material-ui/core";
 import CreateRoomPage from "./CreateRoomPage";
 
 export default class Room extends Component {
@@ -17,13 +21,13 @@ export default class Room extends Component {
         this.roomCode = this.props.match.params.roomCode; // match is the prop with info about how we go to the component from the react routers
 
         // use room code to request to get its data from the backend
-        this.leaveButtonPressed = this.leaveButtonPressed.bind(this);// leave button
-        this.updateShowSettings = this.updateShowSettings.bind(this); // update showSetting in this.state
+        this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+        this.updateShowSettings = this.updateShowSettings.bind(this);
         this.renderSettingsButton = this.renderSettingsButton.bind(this);
         this.renderSettings = this.renderSettings.bind(this);
         this.getRoomDetails = this.getRoomDetails.bind(this);
         this.authenticateSpotify = this.authenticateSpotify.bind(this);
-        this.getRoomDetails();   // update state 
+        this.getRoomDetails();
     }
 
     // function to get room details
@@ -32,34 +36,51 @@ export default class Room extends Component {
         return fetch("/api/get-room" + "?code=" + this.roomCode)
             .then((response) => {
                 if (!response.ok) {
-                    this.props.leaveRoomCallback(); // so it clears the state in the homepage
-                    this.props.history.push("/");
+                this.props.leaveRoomCallback();
+                this.props.history.push("/");
                 }
-                return response.json() // if ok then continue
-            }).then((data) => {
-                this.setState({ //change the state data
-                    votesToSkip: data.votes_to_skip,
-                    guestCanPause: data.guest_can_pause,
-                    isHost: data.is_host,
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({
+                votesToSkip: data.votes_to_skip,
+                guestCanPause: data.guest_can_pause,
+                isHost: data.is_host,
                 });
-                if (this.state.isHost){
-                    this.authenticateSpotify();    // call here if user is host
-                }   
+                if (this.state.isHost) {
+                this.authenticateSpotify();
+                }
             });
     }
 
     //
     authenticateSpotify() {
-        fetch('/spotify/is-authenticated').then((response) => response.json()).then((data) =>{
-            this.setState({
-                spotifyAuthenticated: data.status});
+        fetch("/spotify/is-authenticated")
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ spotifyAuthenticated: data.status });
+                console.log(data.status);
                 if (!data.status) {
-                    fetch('/spotify/get-auth-url').then((response) => response.json()).then((data) => {
-                        window.location.replace(data.url); // js native.redirect user to url to authenticate app
+                fetch("/spotify/get-auth-url")
+                    .then((response) => response.json())
+                    .then((data) => {
+                    window.location.replace(data.url);
                     });
                 }
-        });
+            });
+    }
 
+    // leave room 
+    // edit: added line 49 to set state of homepage roomCode: null
+    leaveButtonPressed() {
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        };
+        fetch("/api/leave-room", requestOptions).then((_response) => {
+          this.props.leaveRoomCallback();
+          this.props.history.push("/");
+        });
     }
 
     // updateShowSettings(value) {
@@ -68,23 +89,36 @@ export default class Room extends Component {
     //     });
     // }
     updateShowSettings(value) {
-        this.setState( {
-            showSettings: value,
+        this.setState({
+          showSettings: value,
         });
     }
 
-    // leave room 
-    // edit: added line 49 to set state of homepage roomCode: null
-    leaveButtonPressed() {
-        // call endpoint /api/leave-room
-        const requestOptions = {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-        };
-        fetch("/api/leave-room", requestOptions).then((_response) => {
-            this.props.leaveRoomCallback(); // so it clears the state in the homepage
-            this.props.history.push("/");    // code redirect to homepage
-        });
+    // render what the settings are
+    // add grid container because it is its own page
+    renderSettings() {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <CreateRoomPage
+                        update={true}
+                        votesToSkip={this.state.votesToSkip}
+                        guestCanPause={this.state.guestCanPause}
+                        roomCode={this.roomCode}
+                        updateCallback={this.getRoomDetails}
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => this.updateShowSettings(false)}
+                    >
+                        Close
+                    </Button>
+                </Grid>
+            </Grid>
+        );
     }
 
     // method that returns html settings
@@ -98,29 +132,6 @@ export default class Room extends Component {
             </Grid>
         );
     }
-
-    // render what the settings are
-    // add grid container because it is its own page
-    renderSettings() {
-        return(
-        <Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-                <CreateRoomPage 
-                    update={true} 
-                    votesToskip={this.state.votesToSkip} 
-                    guestCanPause={this.state.guestCanPause} 
-                    roomCode={this.roomCode} 
-                    updateCallback={this.getRoomDetails}
-                />
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Button variant="contained" color="secondary" onClick={() => this.updateShowSettings(false)}>
-                    Close Settings
-                </Button>
-            </Grid>
-        </Grid>
-        );
-    } 
 
     render() {
         // render this view when updateShowsSettings is true and showSettings is true
@@ -166,3 +177,166 @@ export default class Room extends Component {
 // <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
 // <p>Host: {this.state.isHost.toString()}</p>
 // </div>
+
+// import React, { Component } from "react";
+// import {
+//   Grid,
+//   Button,
+//   Typography,
+//   responsiveFontSizes,
+// } from "@material-ui/core";
+// import CreateRoomPage from "./CreateRoomPage";
+
+// export default class Room extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       votesToSkip: 2,
+//       guestCanPause: false,
+//       isHost: false,
+//       showSettings: false,
+//       spotifyAuthenticated: false,
+//     };
+//     this.roomCode = this.props.match.params.roomCode;
+//     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+//     this.updateShowSettings = this.updateShowSettings.bind(this);
+//     this.renderSettingsButton = this.renderSettingsButton.bind(this);
+//     this.renderSettings = this.renderSettings.bind(this);
+//     this.getRoomDetails = this.getRoomDetails.bind(this);
+//     this.authenticateSpotify = this.authenticateSpotify.bind(this);
+//     this.getRoomDetails();
+//   }
+
+//   getRoomDetails() {
+//     return fetch("/api/get-room" + "?code=" + this.roomCode)
+//       .then((response) => {
+//         if (!response.ok) {
+//           this.props.leaveRoomCallback();
+//           this.props.history.push("/");
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         this.setState({
+//           votesToSkip: data.votes_to_skip,
+//           guestCanPause: data.guest_can_pause,
+//           isHost: data.is_host,
+//         });
+//         if (this.state.isHost) {
+//           this.authenticateSpotify();
+//         }
+//       });
+//   }
+
+//   authenticateSpotify() {
+//     fetch("/spotify/is-authenticated")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         this.setState({ spotifyAuthenticated: data.status });
+//         console.log(data.status);
+//         if (!data.status) {
+//           fetch("/spotify/get-auth-url")
+//             .then((response) => response.json())
+//             .then((data) => {
+//               window.location.replace(data.url);
+//             });
+//         }
+//       });
+//   }
+
+//   leaveButtonPressed() {
+//     const requestOptions = {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//     };
+//     fetch("/api/leave-room", requestOptions).then((_response) => {
+//       this.props.leaveRoomCallback();
+//       this.props.history.push("/");
+//     });
+//   }
+
+//   updateShowSettings(value) {
+//     this.setState({
+//       showSettings: value,
+//     });
+//   }
+
+//   renderSettings() {
+//     return (
+//       <Grid container spacing={1}>
+//         <Grid item xs={12} align="center">
+//           <CreateRoomPage
+//             update={true}
+//             votesToSkip={this.state.votesToSkip}
+//             guestCanPause={this.state.guestCanPause}
+//             roomCode={this.roomCode}
+//             updateCallback={this.getRoomDetails}
+//           />
+//         </Grid>
+//         <Grid item xs={12} align="center">
+//           <Button
+//             variant="contained"
+//             color="secondary"
+//             onClick={() => this.updateShowSettings(false)}
+//           >
+//             Close
+//           </Button>
+//         </Grid>
+//       </Grid>
+//     );
+//   }
+
+//   renderSettingsButton() {
+//     return (
+//       <Grid item xs={12} align="center">
+//         <Button
+//           variant="contained"
+//           color="primary"
+//           onClick={() => this.updateShowSettings(true)}
+//         >
+//           Settings
+//         </Button>
+//       </Grid>
+//     );
+//   }
+
+//   render() {
+//     if (this.state.showSettings) {
+//       return this.renderSettings();
+//     }
+//     return (
+//       <Grid container spacing={1}>
+//         <Grid item xs={12} align="center">
+//           <Typography variant="h4" component="h4">
+//             Code: {this.roomCode}
+//           </Typography>
+//         </Grid>
+//         <Grid item xs={12} align="center">
+//           <Typography variant="h6" component="h6">
+//             Votes: {this.state.votesToSkip}
+//           </Typography>
+//         </Grid>
+//         <Grid item xs={12} align="center">
+//           <Typography variant="h6" component="h6">
+//             Guest Can Pause: {this.state.guestCanPause.toString()}
+//           </Typography>
+//         </Grid>
+//         <Grid item xs={12} align="center">
+//           <Typography variant="h6" component="h6">
+//             Host: {this.state.isHost.toString()}
+//           </Typography>
+//         </Grid>
+//         {this.state.isHost ? this.renderSettingsButton() : null}
+//         <Grid item xs={12} align="center">
+//           <Button
+//             variant="contained"
+//             color="secondary"
+//             onClick={this.leaveButtonPressed}
+//           >
+//             Leave Room
+//           </Button>
+//         </Grid>
+//       </Grid>
+//     );
+//   }
+// }
