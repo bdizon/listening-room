4c8c38799d128ba486484763178d0e88ce4bbbf3
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-23 21:41:57 -0600

    path urls for play and pause endpoints

--- room_controller/spotify/urls.py
+++ room_controller/spotify/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('redirect', spotify_callback), # hit function, redirect to diff view (original app webpage)
     path('is-authenticated', IsAuthenticated.as_view()), # endpoint to IsAuthenticated
     path('current-song', CurrentSong.as_view()),    # endpoint to CurrentSong APIView, returns json of spotify info
+    path('pause', PauseSong.as_view()),
+    path('play', PlaySong.as_view()),
 
 ]
