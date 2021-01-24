b98e45e3f02c3fae90774c00128d5fc1b0c7b1ff
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-23 20:40:50 -0600

    added render MusicPlayer component

--- room_controller/frontend/src/components/Room.js
+++ room_controller/frontend/src/components/Room.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
-import {
-  Grid,
-  Button,
-  Typography,
-  responsiveFontSizes,
-} from "@material-ui/core";
+import { Grid, Button, Typography } from "@material-ui/core";
 import CreateRoomPage from "./CreateRoomPage";
+import MusicPlayer from "./MusicPlayer";
 
 export default class Room extends Component {
     constructor(props) {
@@ -16,6 +12,7 @@ export default class Room extends Component {
             isHost: false,
             showSettings: false,    // use to indicate if settings option should be shown
             spotifyAuthenticated: false,
+            song: {}, // store all info in song in state of component so it is updated with state
         };
 
         this.roomCode = this.props.match.params.roomCode; // match is the prop with info about how we go to the component from the react routers
@@ -27,8 +24,25 @@ export default class Room extends Component {
         this.renderSettings = this.renderSettings.bind(this);
         this.getRoomDetails = this.getRoomDetails.bind(this);
         this.authenticateSpotify = this.authenticateSpotify.bind(this);
+        this.getCurrentSong = this.getCurrentSong.bind(this);
         this.getRoomDetails();
+        // this.getCurrentSong();
     }
+
+    // spotify did not implement web sockets so we cant expect to continuously get updates on the requested data 
+    // solution is polling, but not optimal solution bc this is a lot of requests, but for this website size it should work
+    // set up an interval to update
+    // lifecycle method
+    // component mounts on the screen, then do work
+    componentDidMount() {
+        this.interval = setInterval(this.getCurrentSong, 1000);   // built-in, delay is 1000 ms = 1 second
+    }
+
+    // clean up operatios, have to stop interval 
+    componentWillUnmount() {
+        clearInterval(this.interval);   // stop interval from running
+    }
+
 
     // function to get room details
     // edit: make sure response is valid
@@ -67,8 +81,36 @@ export default class Room extends Component {
                     window.location.replace(data.url);
                     });
                 }
-            });
+        });
     }
+
+    // method to get info from response from /spotify/current-song
+    getCurrentSong() {
+        fetch("/spotify/current-song").then((response) => {
+            if (!response.ok) {
+                return {};
+            } else {
+                return response.json();
+            }
+          })
+          .then((data) => {
+            this.setState({ song: data });
+            console.log(data);
+          });
+      }
+    
+    // getCurrentSong() {
+    //     fetch('/spotify/current-song').then((response) => {
+    //         if (!response.ok) {
+    //             return {};
+    //         } else {
+    //             return response.json();
+    //         }
+    //     }).then((data) => {
+    //         this.setState({song: data});    // setState of song with song data
+    //         console.log(data);
+    //     }); 
+    // }
 
     // leave room 
     // edit: added line 49 to set state of homepage roomCode: null
@@ -145,21 +187,9 @@ export default class Room extends Component {
                         Code: {this.roomCode}
                     </Typography>
                 </Grid>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h6" component="h6">
-                        Votes: {this.state.votesToSkip}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h6" component="h6">
-                        Guest Can Pause: {this.state.guestCanPause.toString()}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h6" component="h6">
-                        Host: {this.state.isHost.toString()}
-                    </Typography>
-                </Grid>
+                {/* {this.state.song} */}
+                {/* spread operator, will pass the props to the MusicPlayer */}
+                <MusicPlayer {...this.state.song} />
                 {this.state.isHost ? this.renderSettingsButton() : null}    
                 <Grid item xs={12} align="center">
                     <Button variant="contained" color="secondary" onClick={this.leaveButtonPressed}>
@@ -170,6 +200,44 @@ export default class Room extends Component {
         
         );
     }
+
+    // render() {
+    //     // render this view when updateShowsSettings is true and showSettings is true
+    //     if (this.state.showSettings) {
+    //         return this.renderSettings();
+    //     }
+    //     return (
+    //         <Grid container spacing={1}>
+    //             <Grid item xs={12} align="center">
+    //                 <Typography variant="h4" component="h4">
+    //                     Code: {this.roomCode}
+    //                 </Typography>
+    //             </Grid>
+    //             <Grid item xs={12} align="center">
+    //                 <Typography variant="h6" component="h6">
+    //                     Votes: {this.state.votesToSkip}
+    //                 </Typography>
+    //             </Grid>
+    //             <Grid item xs={12} align="center">
+    //                 <Typography variant="h6" component="h6">
+    //                     Guest Can Pause: {this.state.guestCanPause.toString()}
+    //                 </Typography>
+    //             </Grid>
+    //             <Grid item xs={12} align="center">
+    //                 <Typography variant="h6" component="h6">
+    //                     Host: {this.state.isHost.toString()}
+    //                 </Typography>
+    //             </Grid>
+    //             {this.state.isHost ? this.renderSettingsButton() : null}    
+    //             <Grid item xs={12} align="center">
+    //                 <Button variant="contained" color="secondary" onClick={this.leaveButtonPressed}>
+    //                     Leave Room
+    //                 </Button>
+    //             </Grid>
+    //         </Grid>
+        
+    //     );
+    // }
 }
 // <div>
 // <h3>{this.roomCode}</h3>
@@ -178,165 +246,3 @@ export default class Room extends Component {
 // <p>Host: {this.state.isHost.toString()}</p>
 // </div>
 
-// import React, { Component } from "react";
-// import {
-//   Grid,
-//   Button,
-//   Typography,
-//   responsiveFontSizes,
-// } from "@material-ui/core";
-// import CreateRoomPage from "./CreateRoomPage";
-
-// export default class Room extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       votesToSkip: 2,
-//       guestCanPause: false,
-//       isHost: false,
-//       showSettings: false,
-//       spotifyAuthenticated: false,
-//     };
-//     this.roomCode = this.props.match.params.roomCode;
-//     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
-//     this.updateShowSettings = this.updateShowSettings.bind(this);
-//     this.renderSettingsButton = this.renderSettingsButton.bind(this);
-//     this.renderSettings = this.renderSettings.bind(this);
-//     this.getRoomDetails = this.getRoomDetails.bind(this);
-//     this.authenticateSpotify = this.authenticateSpotify.bind(this);
-//     this.getRoomDetails();
-//   }
-
-//   getRoomDetails() {
-//     return fetch("/api/get-room" + "?code=" + this.roomCode)
-//       .then((response) => {
-//         if (!response.ok) {
-//           this.props.leaveRoomCallback();
-//           this.props.history.push("/");
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         this.setState({
-//           votesToSkip: data.votes_to_skip,
-//           guestCanPause: data.guest_can_pause,
-//           isHost: data.is_host,
-//         });
-//         if (this.state.isHost) {
-//           this.authenticateSpotify();
-//         }
-//       });
-//   }
-
-//   authenticateSpotify() {
-//     fetch("/spotify/is-authenticated")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         this.setState({ spotifyAuthenticated: data.status });
-//         console.log(data.status);
-//         if (!data.status) {
-//           fetch("/spotify/get-auth-url")
-//             .then((response) => response.json())
-//             .then((data) => {
-//               window.location.replace(data.url);
-//             });
-//         }
-//       });
-//   }
-
-//   leaveButtonPressed() {
-//     const requestOptions = {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//     };
-//     fetch("/api/leave-room", requestOptions).then((_response) => {
-//       this.props.leaveRoomCallback();
-//       this.props.history.push("/");
-//     });
-//   }
-
-//   updateShowSettings(value) {
-//     this.setState({
-//       showSettings: value,
-//     });
-//   }
-
-//   renderSettings() {
-//     return (
-//       <Grid container spacing={1}>
-//         <Grid item xs={12} align="center">
-//           <CreateRoomPage
-//             update={true}
-//             votesToSkip={this.state.votesToSkip}
-//             guestCanPause={this.state.guestCanPause}
-//             roomCode={this.roomCode}
-//             updateCallback={this.getRoomDetails}
-//           />
-//         </Grid>
-//         <Grid item xs={12} align="center">
-//           <Button
-//             variant="contained"
-//             color="secondary"
-//             onClick={() => this.updateShowSettings(false)}
-//           >
-//             Close
-//           </Button>
-//         </Grid>
-//       </Grid>
-//     );
-//   }
-
-//   renderSettingsButton() {
-//     return (
-//       <Grid item xs={12} align="center">
-//         <Button
-//           variant="contained"
-//           color="primary"
-//           onClick={() => this.updateShowSettings(true)}
-//         >
-//           Settings
-//         </Button>
-//       </Grid>
-//     );
-//   }
-
-//   render() {
-//     if (this.state.showSettings) {
-//       return this.renderSettings();
-//     }
-//     return (
-//       <Grid container spacing={1}>
-//         <Grid item xs={12} align="center">
-//           <Typography variant="h4" component="h4">
-//             Code: {this.roomCode}
-//           </Typography>
-//         </Grid>
-//         <Grid item xs={12} align="center">
-//           <Typography variant="h6" component="h6">
-//             Votes: {this.state.votesToSkip}
-//           </Typography>
-//         </Grid>
-//         <Grid item xs={12} align="center">
-//           <Typography variant="h6" component="h6">
-//             Guest Can Pause: {this.state.guestCanPause.toString()}
-//           </Typography>
-//         </Grid>
-//         <Grid item xs={12} align="center">
-//           <Typography variant="h6" component="h6">
-//             Host: {this.state.isHost.toString()}
-//           </Typography>
-//         </Grid>
-//         {this.state.isHost ? this.renderSettingsButton() : null}
-//         <Grid item xs={12} align="center">
-//           <Button
-//             variant="contained"
-//             color="secondary"
-//             onClick={this.leaveButtonPressed}
-//           >
-//             Leave Room
-//           </Button>
-//         </Grid>
-//       </Grid>
-//     );
-//   }
-// }
