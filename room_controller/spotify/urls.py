773db3169785ea1bda047c3a38cf727575bcefc0
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-01-29 16:53:33 -0600

    added path to SkipSong endpoint

--- room_controller/spotify/urls.py
+++ room_controller/spotify/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('current-song', CurrentSong.as_view()),    # endpoint to CurrentSong APIView, returns json of spotify info
     path('pause', PauseSong.as_view()),
     path('play', PlaySong.as_view()),
+    path('skip', SkipSong.as_view()),
 
 ]
